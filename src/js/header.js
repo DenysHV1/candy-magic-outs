4a36284748cbc9251dd88f7c457ce988dfb9b55b
DenVik <denyagarkusha2@gmail.com>
2025-04-26 11:54:00 +0200

    header

--- src/js/header.js
+++ src/js/header.js
@@ -1,27 +1,16 @@
 const mobileMenu = () => {
   const mobileMenuButton = document.querySelector('.header-nav-mobile-button');
   const mobileMenu = document.querySelector('.header-nav-mobile');
-  const mobileMenuButtonClose = document.querySelector(
-    '.header-nav-mobile-button-close'
-  );
   const mobileMenuItems = document.querySelectorAll('.header-nav-mobile-item');
-  const header = document.querySelector('.header');
-  mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.add('is-open');
-  });
 
-  mobileMenuButtonClose.addEventListener('click', () => {
-    mobileMenu.classList.remove('is-open');
+  mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('is-open');
   });
 
   mobileMenuItems.forEach(item => {
     item.addEventListener('click', () => {
       mobileMenu.classList.remove('is-open');
     });
-  });
-
-  window.addEventListener('scroll', () => {
-    header.classList.toggle('scroll', window.scrollY > 0);
   });
 };
 
